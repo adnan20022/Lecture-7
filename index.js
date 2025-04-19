34d70a0fe2bf15f9945b1deaf353767583cdafd2
adnan20022 <mu209367@gmail.com>
2025-04-19 08:32:46 -0700

    Function in js

---
+++ index.js
@@ -0,0 +1,57 @@
+// Function
+function sum(x , y){
+    console.log(x+y);
+}
+sum(2 , 3);
+
+function subs(x , y){
+    console.log(x-y);
+}
+subs(3 , 4);
+
+function login(email, password){
+    console.log("email",email);
+    console.log("password",password);
+
+}
+
+login("madnanali@gmail.com", "2234566")
+function sayGreat(){
+    console.log("asdf");
+}
+   
+sayGreat();
+function sum(x , y){
+    s = x + y;
+    return s;
+}
+sum(2 , 3);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
